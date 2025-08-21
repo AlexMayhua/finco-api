8254062b1a13f0d154d9fe49b29619c24823851a
Alexmayhua <alexmayhuaquispe@gmail.com>
2025-08-21 17:39:03 -0500

    feat: mover rutas de perfil a un grupo separado y agregar métodos de actualización

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,7 +14,12 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('me', [AuthController::class, 'me']);
         Route::post('logout', [AuthController::class, 'logout']);
     });
+});
+
+Route::middleware('auth:sanctum')->prefix('profile')->group(function () {
+    Route::get('me', [ProfileController::class, 'me']);
+    Route::put('/', [ProfileController::class, 'update']);
+    Route::patch('/', [ProfileController::class, 'update']);
 });
