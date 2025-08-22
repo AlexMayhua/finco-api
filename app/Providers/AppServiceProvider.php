1eb8019fe63e24b389315e416688178ee8fabc2d
Alexmayhua <alexmayhuaquispe@gmail.com>
2025-08-21 20:38:01 -0500

    feat: registrar ProfileRepositoryInterface y ProfileServiceInterface en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,10 @@ use App\Contracts\UserRepositoryInterface;
 use App\Repositories\UserRepository;
 use App\Contracts\AuthServiceInterface;
 use App\Services\AuthService;
+use App\Contracts\ProfileRepositoryInterface;
+use App\Repositories\ProfileRepository;
+use App\Contracts\ProfileServiceInterface;
+use App\Services\ProfileService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
+        $this->app->bind(ProfileRepositoryInterface::class, ProfileRepository::class);
+        $this->app->bind(ProfileServiceInterface::class, ProfileService::class);
     }
 
     /**
